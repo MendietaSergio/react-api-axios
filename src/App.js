d6b86a379ae0a121930f0ab74ce7283fe8f0b865
Mendieta Sergio <mendieta.sergio.1995@gmail.com>
2021-02-13 11:16:44 -0300

    Añade modificacion en App.js y api.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {Component} from 'react';
+import api from './api';
 
-function App() {
-  return (
-    <div className="App container">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  
+  state = {
+    filmes: [],
+  }
+  async componentDidMount(){
+    const response = await api.get('marvel');
+
+    this.setState({filmes : response.data});
+  }
+  render(){
+    //almacena la data del endpoint
+    const {filmes} = this.state;
+    return (
+      <div className="container">
+        <h1 className="text-center mt-5">Peliculas de Marvel</h1>
+        <div className="row">
+            {filmes.map(filme =>
+              <h5>{filme.show.name}</h5>
+            )}
+        </div>
+      
+      </div>
+    )
+  }
 }
 
 export default App;
